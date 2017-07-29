f576a3b5bc169ca29fe5624ee9a082b5d126d9c0
João Vitor Alves <joao.vitor@redealumni.com>
2017-07-29 19:15:41 -0300

    check if path exists after pass to files select

--- app/controllers/morion/morion_controller.rb
+++ app/controllers/morion/morion_controller.rb
@@ -5,9 +5,12 @@ module Morion
     def index
       root = Rails.root.to_s
       regex = /.*(\.jpg|\.jpeg|\.png|\.svg)/
-      app_assets_paths = Rails.application.assets.paths.
-        select {|path| path =~ /#{root}/}.
-        map    {|path| Pathname.new(path).cleanpath }
+
+      app_assets_paths = Rails.application.assets.paths.select {|path| path =~ /#{root}/}
+      app_assets_paths = app_assets_paths.map do |path|
+        pathname = Pathname.new(path)
+        pathname.exist? ? pathname.cleanpath : nil
+      end.compact
 
       files_data = Find.find(*app_assets_paths).select {|path| path =~ regex }
 
@@ -23,8 +26,6 @@ module Morion
 
         {filename: filename, asset_path: asset_path, path: folder_path, extension: extension}
       end
-
-      # binding.pry
 
       @folders = files_data.group_by {|file| file[:path].match(/(.*\/).*/)[1] }
     end
