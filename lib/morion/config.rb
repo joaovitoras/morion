cc1dd76e74b1cbca6ed0f31da4ac29e139f26cf9
João Vitor Alves <joao.vitor@redealumni.com>
2017-08-18 13:55:08 -0300

    adds black and white list accessors

--- lib/morion/config.rb
+++ lib/morion/config.rb
@@ -1,6 +1,12 @@
 module Morion
   module Config
     class << self
+      # Configuration option to specify a blacklist of folders you want to Morion don't work with.
+      attr_accessor :blacklist
+
+      # Configuration option to specify a whitelist of folders you want to Morion to work with.
+      attr_accessor :whitelist
+
     end
   end
 end
