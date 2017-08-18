be08f2b11937ddb259a9f690885eebafe2b9e947
João Vitor Alves <joao.vitor@redealumni.com>
2017-08-18 13:55:28 -0300

    adds method to set default values to config

--- lib/morion/config.rb
+++ lib/morion/config.rb
@@ -7,6 +7,12 @@ module Morion
       # Configuration option to specify a whitelist of folders you want to Morion to work with.
       attr_accessor :whitelist
 
+      def set_default_values
+        @blacklist = []
+        @whitelist = []
+      end
     end
+
+    set_default_values
   end
 end
